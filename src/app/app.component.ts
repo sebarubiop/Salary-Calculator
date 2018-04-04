f0f86a342332f8df655f2f7b4de4899601facaec
sebarubiop <sebarubiop@gmail.com>
2018-04-04 14:07:10 +1000

    calculate salary and tax methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,49 +8,84 @@ import { MatTableDataSource } from '@angular/material';
 })
 export class AppComponent {
   title = "Tax Estimation Calculator";
-  income: number;
+  income: number = 50000;
+  newIncome: number;
   types = [
     { id: 0, name: "Gross" },
     { id: 1, name: "Gross + Superannuation" }];
   type: number = 0;
+  superAmount: number;
+  superPercentage: number = 9.5;
+  taxRate: number;
+  netSalary: number;
+  netSuper: number;
   showSpinner: boolean = false;
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns = ['superannuation', 'gross', 'grossSuper', 'tax', 'net', 'netSuper'];
+  ELEMENT_DATA: Element[];
+  dataSource;
 
   calculateSalary() {
-    this.showSpinner = true;
+    // clean the datatable
+    this.ELEMENT_DATA = [];
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
 
-    console.log(this.income + ' ' + this.type);
+    this.showSpinner = true;
+    
+    //Simulating a rest api request
+    setTimeout(() => {
+
+      if(this.type == 0){ //Gross (i.e. $50,000)
+        this.superAmount = this.income * (this.superPercentage / 100);// (i.e. $50,000 * 9.5% = $4,750)
+        this.taxRate = this.calculateTax(this.income);// (i.e. $7,797 - after calculateTax)
+        this.netSalary = this.income - this.taxRate;// (i.e. $50,000 - $7,797 = $42,203)
+        this.netSuper = this.netSalary + this.superAmount;
+        this.ELEMENT_DATA = [
+          {superannuation: this.superAmount, gross:this.income, grossSuper:this.income+this.superAmount, tax: this.taxRate, net: this.netSalary, netSuper: this.netSuper}];
+        this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+
+      }else if(this.type == 1){ //Gross + SuperAnnuation
+        this.superAmount = this.income * (this.superPercentage / 100);// (i.e. $50,000 * 9.5% = $4,750)
+        this.newIncome = this.income - this.superAmount;// (i.e. $50,000 - $4,750 = $45,250)
+        this.taxRate = this.calculateTax(this.newIncome);// (i.e. $6,253.25 - after calculateTax)
+        this.netSalary = this.newIncome - this.taxRate;// (i.e. $45,250 - $6,253.25 = $38,996.75)
+        this.netSuper = this.netSalary + this.superAmount;
+        this.ELEMENT_DATA = [
+          {superannuation: this.superAmount, gross:this.newIncome, grossSuper:this.income, tax: this.taxRate, net: this.netSalary, netSuper: this.netSuper}];
+        this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      } 
+
+      this.showSpinner = false;
+      
+    }, 1000);
+  }
+
+  calculateTax(amount): number {
+    let tax: number, amountDiff: number;
+    if(amount <= 18200)
+      return 0;
+    if(amount >= 18201 && amount <= 37000)
+      tax = amount * 0.19;
+    if(amount >= 37001 && amount <= 87000){
+      amountDiff = amount - 37000;
+      tax = (amountDiff * 0.325) + 3572;
+    }
+    if(amount >= 87001 && amount <= 180000){
+      amountDiff = amount - 87000;
+      tax = (amountDiff * 0.37) + 19822;
+    }
+    if(amount >= 180001){
+      amountDiff = amount - 180000;
+      tax = (amountDiff * 0.45) + 54232;
+    }
+    return tax;
   }
 }
 
-
 export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  superannuation: number;
+  gross?: number;
+  grossSuper?: number;
+  tax: number;
+  net: number;
+  netSuper: number;
 }
-
-const ELEMENT_DATA: Element[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
